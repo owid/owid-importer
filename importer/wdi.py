ccbaf1ae893d6f6aff73b687b456785f55dff9c2
Daniel Gavrilov <dgavrilov95@gmail.com>
2018-11-27 17:44:08 +0000

    Move away from _importer suffix

--- importer/wdi.py
+++ importer/wdi.py
@@ -38,7 +38,7 @@ DATA_EXPECTED_HEADERS = ('Country Name', 'Country Code', 'Indicator Name', 'Indi
 COUNTRY_EXPECTED_HEADERS = ('Country Code', 'Short Name', 'Table Name', 'Long Name', '2-alpha code', 'Currency Unit', 'Special Notes', 'Region', 'Income Group', 'WB-2 code', 'National accounts base year', 'National accounts reference year', 'SNA price valuation', 'Lending category', 'Other groups', 'System of National Accounts', 'Alternative conversion factor', 'PPP survey year', 'Balance of Payments Manual in use', 'External debt Reporting status', 'System of trade', 'Government Accounting concept', 'IMF data dissemination standard', 'Latest population census', 'Latest household survey', 'Source of most recent Income and expenditure data', 'Vital registration complete', 'Latest agricultural census', 'Latest industrial data', 'Latest trade data')
 
 logging.basicConfig(
-    filename=os.path.join('..', 'logs', 'wdi_importer.log'),
+    filename=os.path.join('..', 'logs', 'wdi.log'),
     level=logging.DEBUG,
     format='%(asctime)s [%(levelname)s] %(name)s: %(message)s'
 )
