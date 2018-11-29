fc6cd30b225d02520e72f0594b1c44e70095b7ee
Daniel Gavrilov <dgavrilov95@gmail.com>
2018-11-29 17:13:00 +0000

    Prefix log filenames with database name

--- importer/wdi.py
+++ importer/wdi.py
@@ -39,7 +39,7 @@ DATA_EXPECTED_HEADERS = ('Country Name', 'Country Code', 'Indicator Name', 'Indi
 COUNTRY_EXPECTED_HEADERS = ('Country Code', 'Short Name', 'Table Name', 'Long Name', '2-alpha code', 'Currency Unit', 'Special Notes', 'Region', 'Income Group', 'WB-2 code', 'National accounts base year', 'National accounts reference year', 'SNA price valuation', 'Lending category', 'Other groups', 'System of National Accounts', 'Alternative conversion factor', 'PPP survey year', 'Balance of Payments Manual in use', 'External debt Reporting status', 'System of trade', 'Government Accounting concept', 'IMF data dissemination standard', 'Latest population census', 'Latest household survey', 'Source of most recent Income and expenditure data', 'Vital registration complete', 'Latest agricultural census', 'Latest industrial data', 'Latest trade data')
 
 logging.basicConfig(
-    filename=os.path.join(CURRENT_PATH, '..', 'logs', 'wdi.log'),
+    filename=os.path.join(CURRENT_PATH, '..', 'logs', '%s-wdi.log' % (os.environ['DB_NAME'])),
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(name)s: %(message)s'
 )
